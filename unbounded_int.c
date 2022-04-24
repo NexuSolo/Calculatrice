87bc2edc5c1617320325cae4b6dff5e147ab44ce
THEAU Nicolas <Theaun91@gmail.com>
2022-04-24 17:09:53 +0200

    5 premiere fonctiosn finis + correction

--- unbounded_int.c
+++ unbounded_int.c
@@ -1,21 +1,37 @@
 #include "unbounded_int.h"
+
 #include <assert.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <stdbool.h>
 
-unbounded_int *init_unbounded_int() {
-    unbounded_int *res = malloc(sizeof(unbounded_int));
-    assert(res != NULL);
-    // if(res == NULL) { return unbounded_int a = {.signe = '*', .premier = NULL, .dernier = NULL, .len = 0}; }
-    res->premier = NULL;
-    res->dernier = NULL;
-    res->len = 0;
-    res->signe = '+';
-    return res;
+unbounded_int init_unbounded_int() {
+    return (unbounded_int) {.signe = '+', .len = 0, .premier = NULL, .dernier = NULL};
 }
 
-void add_chiffre_to_unbounded_int_at_end(unbounded_int *n, char c) {
+void add_char_to_unbounded_int_at_start(unbounded_int *n, char c) {
     chiffre *ch = char_to_chiffre(c);
+    if (ch == NULL || n->signe == '*') {
+        *n = unbounded_int_error;
+        return;
+    }
+    if(n->premier == NULL) {
+        n->premier = n->dernier = ch;
+        n->len = 1;
+        return;
+    }
+    ch->suivant = n->premier;
+    n->premier->precedent = ch;
+    n->premier = ch;
+    n->len++;
+}
+
+void add_char_to_unbounded_int_at_end(unbounded_int *n, char c) {
+    chiffre *ch = char_to_chiffre(c);
+    if (ch == NULL || n->signe == '*') {
+        *n = unbounded_int_error;
+        return;
+    }
     if(n->premier == NULL) {
         n->premier = n->dernier = ch;
         n->len = 1;
@@ -29,27 +45,58 @@ void add_chiffre_to_unbounded_int_at_end(unbounded_int *n, char c) {
 
 chiffre *char_to_chiffre(char c) {
     chiffre *res = malloc(sizeof(chiffre));
-    assert(res != NULL);
+    if (res == NULL) {
+        return NULL;
+    }
     res->c = c;
     res->precedent = res->suivant = NULL;
     return res;
 }
 
 unbounded_int string2unbounded_int(const char *e) {
-    unbounded_int *res = init_unbounded_int();
+    unbounded_int res = init_unbounded_int();
     size_t i = 0;
+    bool premier = true;
     if(e[i] == '+' || e[i] == '-') {
-        res->signe = e[i];
+        res.signe = e[i];
         i++;
     }
     while (e[i] != '\0') {
-        if (!isdigit(e[i])) {
-            
+        if (isdigit(e[i])) {
+            if(!premier) {
+                add_char_to_unbounded_int_at_end(&res, e[i]);
+            }
+            else if (e[i] != '0') {
+                add_char_to_unbounded_int_at_end(&res, e[i]);
+                premier = false;
+            }
+            else if (e[i + 1] == '\0') {
+                add_char_to_unbounded_int_at_end(&res, e[i]);
+            }
+            i++;
         }
-        add_chiffre_to_unbounded_int_at_end(res, e[i]);
-        i++;
+        else {
+            return unbounded_int_error;
+        }
     }
-    return *res;
+    return res;
+}
+
+unbounded_int ll2unbounded_int(long long i) {
+    unbounded_int res = init_unbounded_int();
+    if(i == 0) {
+        add_char_to_unbounded_int_at_end(&res, '0');
+        return res;
+    }
+    if(i < 0) {
+        res.signe = '-';
+        i = -i;
+    }
+    while(i > 0) {
+        add_char_to_unbounded_int_at_start(&res, i % 10 + '0');
+        i /= 10;
+    }
+    return res;
 }
 
 char *unbounded_int2string(unbounded_int n) {
@@ -60,8 +107,13 @@ char *unbounded_int2string(unbounded_int n) {
         res[1] = '\0';
         return res;
     }
-    res[0] = n.signe;
-    size_t i = 1;
+    size_t i = 0;
+    if(n.signe == '-') {
+        if(n.premier->c != '0') {
+            res[0] = n.signe;
+            i++;
+        }
+    }
     chiffre *tmp = n.premier;
     while (tmp != NULL) {
         res[i] = tmp->c;
@@ -72,36 +124,37 @@ char *unbounded_int2string(unbounded_int n) {
     return res;
 }
 
-void add_chiffre_to_unbounded_int_at_start(unbounded_int *n, char c) {
-    chiffre *ch = char_to_chiffre(c);
-    if(n->premier == NULL) {
-        n->premier = n->dernier = ch;
-        n->len = 1;
-        return;
+int unbounded_int_cmp_unbounded_int(unbounded_int a, unbounded_int b) {
+    if (a.signe == b.signe) {
+        if (a.len != b.len) {
+            if(a.signe == '+') {
+                return a.len < b.len ?-1:1;
+            }
+            return a.len < b.len ?1:-1;
+        }
+        chiffre *tmpa = a.premier;
+        chiffre *tmpb = b.premier;
+        for (int i = 0; i < a.len; i++) {
+            if(tmpa->c != tmpb->c) {
+                return tmpa->c < tmpb->c ?-1:1;
+            }
+            tmpa = tmpa->suivant;
+            tmpb = tmpb->suivant;
+        }
+        return 0;
     }
-    ch->suivant = n->premier;
-    n->premier->precedent = ch;
-    n->premier = ch;
-    n->len++;
+    else {
+        return a.signe == '+' ?1:-1;
+    }
 }
 
-//prend un long long en parametre et renvoi un unbounded_int
-unbounded_int ll2unbounded_int(long long i) {
-    unbounded_int *res = init_unbounded_int();
-    if(i < 0) {
-        res->signe = '-';
-        i = -i;
-    }
-    while(i > 0) {
-        add_chiffre_to_unbounded_int_at_start(res, i % 10 + '0');
-        i /= 10;
-    }
-    return *res;
+int unbounded_int_cmp_ll(unbounded_int a, long long b) {
+    return unbounded_int_cmp_unbounded_int(a,ll2unbounded_int(b));
 }
 
 int main(int argc, char const *argv[]) {
-    unbounded_int t = unbounded_int_error;
-    unbounded_int n = string2unbounded_int("+123456789");
-    printf("%s\n", unbounded_int2string(n));
+    unbounded_int n = string2unbounded_int("100");
+    long long n2 = -10;
+    printf("%d\n", unbounded_int_cmp_ll(n, n2));
     return 0;
 }
--- unbounded_int.h
+++ unbounded_int.h
@@ -13,16 +13,20 @@ typedef struct {
     chiffre *dernier;
 } unbounded_int;
 
-static unbounded_int *init_unbounded_int();
+static unbounded_int init_unbounded_int();
 
-static void add_chiffre_to_unbounded_int_at_end(unbounded_int *n, char c);
+static void add_char_to_unbounded_int_at_end(unbounded_int *n, char c);
 
-static void add_chiffre_to_unbounded_int_at_start(unbounded_int *n, char c);
+static void add_char_to_unbounded_int_at_start(unbounded_int *n, char c);
 
 static chiffre *char_to_chiffre(char c);
 
 static unbounded_int string2unbounded_int(const char *e);
 
 static unbounded_int ll2unbounded_int(long long i);
+
+static int unbounded_int_cmp_unbounded_int(unbounded_int a, unbounded_int b);
+
+static int unbounded_int_cmp_ll(unbounded_int a, long long b);
 
 static unbounded_int unbounded_int_error = {.signe = '*', .len = 0, .premier = NULL, .dernier = NULL};
